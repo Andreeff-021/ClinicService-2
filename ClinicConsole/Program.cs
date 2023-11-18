e56055d937745588d1da2bb6096a5773849388b6
andre <andre@LAPTOP-Q8HPBVSM>
2023-11-18 23:09:19 +0300

    Добавьте файлы проекта.

---
+++ ClinicConsole/Program.cs
@@ -0,0 +1,114 @@
+﻿using ClinicServiceNamespace;
+using System.Data;
+
+namespace ClinicConsole
+{
+
+    partial class Program
+    {
+
+    }
+
+    partial class Program
+    {
+        static void DoProcess()
+        {
+
+        }
+
+    }
+
+    partial class Program
+    {
+
+    }
+
+    partial class Program
+    {
+
+    }
+
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Нажмите 1, чтобы загрузить всех клиентов; " +
+                    "\n2 - чтобы удалить клиента; " +
+                    "\n3 - Создать клиента; " +
+                    "\n4 - обновить клиента; " +
+                    "\n5 - выйти");
+
+                String res = Console.ReadLine();
+
+
+                ClinicClient clinicClient = new ClinicClient("http://localhost:5115/", new HttpClient());
+
+                if (res == "1")
+                {
+                    List<Client> clients = clinicClient.ClientGetAllAsync().Result.ToList();
+                    foreach (Client client in clients)
+                    {
+                        Console.WriteLine("Фамилия: " + client.SurName);
+                        Console.WriteLine("Имя: " + client.FirstName);
+                        Console.WriteLine("Отчество: " + client.Patronymic);
+                        Console.WriteLine("Дата рождения: " + client.Birthday.DateTime);
+                        Console.WriteLine("Документ: " + client.Document);
+                        Console.WriteLine();
+                    }
+                }
+                else if (res == "2")
+                {
+                    Console.WriteLine("Введите id клиента, которого хотите удалить");                    
+                    int clientId = Int32.Parse(Console.ReadLine());
+                    clinicClient.DeleteAsync(clientId);
+                    Console.WriteLine($"Клиент с id:{clientId} удален");
+                }
+                
+                else if (res == "3")
+                {
+                    CreateClientRequest request = new CreateClientRequest();
+                    Console.WriteLine("Документ: ");
+                    request.Document = Console.ReadLine();
+                    Console.WriteLine("Фамилия: ");
+                    request.SurName = Console.ReadLine();
+                    Console.WriteLine("Имя: ");
+                    request.FirstName = Console.ReadLine();
+                    Console.WriteLine("Отчество: ");
+                    request.Patronymic = Console.ReadLine();
+                    Console.WriteLine("Дата рождения: ");   
+                    request.Birthday = DateTime.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                    clinicClient.CreateAsync(request);
+                }
+
+                else if (res == "4")
+                {
+                    UpdateClientRequest request = new UpdateClientRequest();
+                    Console.WriteLine("Id клиента: ");
+                    request.ClientId = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine("Документ: ");
+                    request.Document = Console.ReadLine();
+                    Console.WriteLine("Фамилия: ");
+                    request.SurName = Console.ReadLine();
+                    Console.WriteLine("Имя: ");
+                    request.FirstName = Console.ReadLine();
+                    Console.WriteLine("Отчество: ");
+                    request.Patronymic = Console.ReadLine();
+                    Console.WriteLine("Дата рождения: ");
+                    request.Birthday = DateTime.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                    clinicClient.EditAsync(request);
+                }
+
+                else if(res == "5")
+                {
+                    break;
+                }
+            }
+            
+
+        }
+    }
+}
---
+++ ClinicDesctop/MainForm.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ClinicDesctop
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listViewClients = new ListView();
+            columnHeaderId = new ColumnHeader();
+            columnHeaderSurName = new ColumnHeader();
+            columnHeaderFirstName = new ColumnHeader();
+            columnHeaderPatronymic = new ColumnHeader();
+            columnHeaderDocument = new ColumnHeader();
+            buttonLoadClients = new Button();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // listViewClients
+            // 
+            listViewClients.Columns.AddRange(new ColumnHeader[] { columnHeaderId, columnHeaderSurName, columnHeaderFirstName, columnHeaderPatronymic, columnHeaderDocument });
+            listViewClients.FullRowSelect = true;
+            listViewClients.GridLines = true;
+            listViewClients.Location = new Point(14, 16);
+            listViewClients.Margin = new Padding(3, 4, 3, 4);
+            listViewClients.MultiSelect = false;
+            listViewClients.Name = "listViewClients";
+            listViewClients.Size = new Size(886, 413);
+            listViewClients.TabIndex = 0;
+            listViewClients.UseCompatibleStateImageBehavior = false;
+            listViewClients.View = View.Details;
+            // 
+            // columnHeaderId
+            // 
+            columnHeaderId.Text = "#";
+            // 
+            // columnHeaderSurName
+            // 
+            columnHeaderSurName.Text = "Фамилия";
+            columnHeaderSurName.Width = 120;
+            // 
+            // columnHeaderFirstName
+            // 
+            columnHeaderFirstName.Text = "Имя";
+            columnHeaderFirstName.Width = 100;
+            // 
+            // columnHeaderPatronymic
+            // 
+            columnHeaderPatronymic.Text = "Отчество";
+            columnHeaderPatronymic.Width = 120;
+            // 
+            // columnHeaderDocument
+            // 
+            columnHeaderDocument.Text = "Документ";
+            columnHeaderDocument.Width = 200;
+            // 
+            // buttonLoadClients
+            // 
+            buttonLoadClients.Location = new Point(776, 439);
+            buttonLoadClients.Margin = new Padding(3, 4, 3, 4);
+            buttonLoadClients.Name = "buttonLoadClients";
+            buttonLoadClients.Size = new Size(125, 68);
+            buttonLoadClients.TabIndex = 1;
+            buttonLoadClients.Text = "Загрузить";
+            buttonLoadClients.UseVisualStyleBackColor = true;
+            buttonLoadClients.Click += buttonLoadClients_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(629, 439);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(125, 68);
+            button1.TabIndex = 2;
+            button1.Text = "Удалить\r\n";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(914, 600);
+            Controls.Add(button1);
+            Controls.Add(buttonLoadClients);
+            Controls.Add(listViewClients);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Моя клиника";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListView listViewClients;
+        private ColumnHeader columnHeaderId;
+        private ColumnHeader columnHeaderSurName;
+        private ColumnHeader columnHeaderFirstName;
+        private ColumnHeader columnHeaderPatronymic;
+        private ColumnHeader columnHeaderDocument;
+        private Button buttonLoadClients;
+        private Button button1;
+    }
+}
